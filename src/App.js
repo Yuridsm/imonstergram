bd2102f2673ab40521637e902b2a02982810b498
Yuri Melo <yurifullstack@gmail.com>
2020-09-20 10:52:17 -0300

    new Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+import { api } from './api/api.js';
+
+class App extends Component {
+   constructor() {
+      super();
+
+      this.state = {
+         monsters: [],
+         searchField: ''
+      }
+   }
+
+   async componentDidMount() {
+      const response = await api.get('');
+      this.setState({ monsters: response.data });
+   }
+
+   render() {
+      const { monsters, searchField } = this.state;
+      const filteredMonsters = monsters.filter(monster =>
+         monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
+      return (
+            <div className="App">
+               <header>
+                  <h1>iMonstergram</h1>
+               </header>
+
+               <SearchBox
+                  placeholder="Search monster"
+                  handleChange={e => this.setState({ searchField: e.target.value })}
+               />
+
+               <CardList monsters={ filteredMonsters } />
+            </div>
+      );
+  }
 }
 
 export default App;
